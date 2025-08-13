db841833a0d629cec72505f908b44853e8a4a8c1
orayozyigit <orayozyigit@gmail.com>
2025-08-13 13:49:43 +0300

    Update card design with new comment

--- src/components/BulkNotification.tsx
+++ src/components/BulkNotification.tsx
@@ -83,7 +83,7 @@ function BulkNotification({ onClose }: BulkNotificationProps) {
         maxWidth: '1200px',
         margin: '0 auto'
       }}>
-        {/* Ana Kart */}
+        {/* Ana Kart - Deploy için güncellendi */}
         <div style={{
           backgroundColor: 'white',
           borderRadius: '24px',
