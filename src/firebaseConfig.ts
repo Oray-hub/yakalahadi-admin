17c23fa7c5905b14ccb21dfeae6dd0de7115377b
orayozyigit <orayozyigit@gmail.com>
2025-08-17 01:09:42 +0300

    storageBucket düzeltildi: .firebasestorage.app -> .appspot.com

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyDL_ZXei88Jo7Xa3Ww4DjjMIPDOiy5j_QM",
   authDomain: "yakalahadi-333ca.firebaseapp.com",
   projectId: "yakalahadi-333ca",
-  storageBucket: "yakalahadi-333ca.firebasestorage.app",
+  storageBucket: "yakalahadi-333ca.appspot.com",
   messagingSenderId: "397021820606",
   appId: "1:397021820606:web:a3aa08c2a558b15fef10ef"
 };
