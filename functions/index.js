26cea461943801a67b62a13c6e59b6fb1bf0eee3
orayozyigit <orayozyigit@gmail.com>
2025-08-17 15:15:33 +0300

    fix: deleteUserCompletely fonksiyonu deploy hatası düzeltildi, gereksiz test paketi kaldırıldı

--- functions/index.js
+++ functions/index.js
@@ -18,10 +18,14 @@ const { userNotification } = require('./userNotification');
 // Firma bildirimi fonksiyonunu import et
 const { companyNotification } = require('./companyNotification');
 
+// Delete user completely fonksiyonunu import et
+const { deleteUserCompletely } = require('./deleteUserCompletely');
+
 // Export the functions
 exports.sendCompanyApprovalEmail = sendCompanyApprovalEmail;
 exports.sendBulkNotificationTrigger = sendBulkNotificationTrigger;
 exports.sendCompanyApprovalNoticeTrigger = sendCompanyApprovalNoticeTrigger;
 exports.userNotification = userNotification;
 exports.companyNotification = companyNotification;
+exports.deleteUserCompletely = deleteUserCompletely;
 
